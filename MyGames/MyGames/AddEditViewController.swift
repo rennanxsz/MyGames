9576ba17c9769e31271a347d2a1011c32266cc79
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-16 22:43:32 -0300

    Creating classes
    Criando as classes.

---
+++ MyGames/MyGames/AddEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddEditViewController.swift
+//  MyGames
+//
+//  Created by Rennan Bruno on 16/02/22.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
